171a081fba14502ce7337e5767fb9507c225ae83
iiRealisticDev <thomaselston8@gmail.com>
2022-04-28 19:44:15 +0100

    Added some options in jsdoc.conf.json and corrected example for database.ts

--- modules/database.ts
+++ modules/database.ts
@@ -9,7 +9,7 @@ export class RealisticDatabase {
    * @param {string} dbDir A path to the directory the DB should be in.
    * @param {DBFile} dbFile A file where the DB should be stored. Must be JSON.
    * @example
-   * const db = new RealisticDatabase("./db", new File("db.json", "{}"));
+   * const db = new RealisticDatabase("./db", new DBFile("db.json", "{}"));
    */
   constructor(dbDir: string, dbFile: DBFile) {
     if (!(dbFile instanceof DBFile)) throw new Error("Database file must be a File.");
